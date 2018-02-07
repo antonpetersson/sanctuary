436e0e3cc6f6fd760ec75b6cdfb7ecf6d3b16a1e
antonpetersson <antonpetersson@users.noreply.github.com>
2018-02-07 14:21:02 +0100

    Man kan välja vilka djur man vill ha.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sanctuary</title>
+</head>
+<body>
+
+<form action="result.php" method="get">
+Välj hur många apor du vill ha: <input type="number" name="monkies"><br>
+Välj hur många giraffer du vill ha: <input type="number" name="giraffes"><br>
+Välj hur många tigrar du vill ha: <input type="number" name="tigers"><br>
+Välj hur många kokosnötter du vill ha: <input type="number" name="coconuts"><br>
+<input type="submit" value="Gå till ditt naturreservat">
+</form>
+
+
+
+    
+</body>
+</html>
